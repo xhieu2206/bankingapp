310fc59390336ce29e9d1db766096999715f50c7
xhieu94 <xhieu94@gmail.com>
2020-06-17 15:55:05 +0700

    init commit

---
+++ src/test/java/fpt/banking/system/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.banking.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
