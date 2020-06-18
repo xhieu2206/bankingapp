a58b855c1f6caa74409aa22d751a034d9bcfde5f
xhieu94 <xhieu94@gmail.com>
2020-06-18 18:32:47 +0700

    Update sql script

--- sql_script/script.sql
+++ sql_script/script.sql
@@ -6,33 +6,24 @@ DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` char(80) NOT NULL,
-  `fullname` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `phone` varchar(50) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(50),
+  `address` varchar(500),
   `status` bool,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-INSERT INTO `user` (username,password,fullname,email,phone,status,address)
-VALUES 
-('user_1','$2y$12$IojDHLSwsag0uk4RPmY1Re7ek/b4ptRNAsPohxsB9DdAEDGUiHMb6','User Name 1','user1@gmail.com','123456789','1'),
-('employee_1','$2a$10$8fIEgmzmL.wyfS/HZP927.QqclDoklNSWuEhjmc7C13quS7KTpitC','Employee Name 1','employee1@gmail.com','123422789','1'),
-('user_2','$2a$10$8fIEgmzmL.wyfS/HZP927.QqclDoklNSWuEhjmc7C13quS7KTpitC','User Name 2','user2@gmail.com','123454589','1');
 
 DROP TABLE IF EXISTS `role`;
 
 CREATE TABLE `role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-
-INSERT INTO `role` (name)
-VALUES 
-('ROLE_USER'),('ROLE_EMPLOYEE'),('ROLE_TRANSACTIONMANAGER'),
-('ROLE_BRANCHMANAGER'),('ROLE_BANKMANAGER');
 
 DROP TABLE IF EXISTS `users_roles`;
 
@@ -53,10 +44,19 @@ CREATE TABLE `users_roles` (
   ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `role` (name)
+VALUES 
+('ROLE_USER'),('ROLE_EMPLOYEE'),('ROLE_TRANSACTIONMANAGER'),
+('ROLE_BRANCHMANAGER'),('ROLE_BANKMANAGER');
 
 INSERT INTO `users_roles` (user_id,role_id)
 VALUES 
 (1, 1),
 (2, 2),
 (3, 1);
+
+INSERT INTO `user` (username,password,fullname,email,phone,status, address)
+VALUES 
+('user_1','$2y$12$IojDHLSwsag0uk4RPmY1Re7ek/b4ptRNAsPohxsB9DdAEDGUiHMb6','User Name 1','user1@gmail.com','123456789', 1, 'Hanoi'),
+('employee_1','$2a$10$8fIEgmzmL.wyfS/HZP927.QqclDoklNSWuEhjmc7C13quS7KTpitC','Employee Name 1','employee1@gmail.com','123422789', 1, 'Hanoi'),
+('user_2','$2a$10$8fIEgmzmL.wyfS/HZP927.QqclDoklNSWuEhjmc7C13quS7KTpitC','User Name 2','user2@gmail.com','123454589', 1, 'Hanoi');
